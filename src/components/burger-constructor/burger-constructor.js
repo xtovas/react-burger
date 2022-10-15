c0c24fa5b089cf8f67c37106eb1f909e0cd21b6f
Sofia Kuptsova <sofia.kuptt39@gmail.com>
2022-10-15 14:31:16 +0200

    fix modal

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,81 +1,82 @@
 import { useState } from "react";
-import React from "react";
 import PropTypes from "prop-types";
-import { FoodPropTypes } from "../../utils/data";
-import { arrayOf } from "prop-types";
-import Modal from "../modal/modal";
-import OrderDetails from "../order-details/order-details";
-import {
-  ConstructorElement,
-  DragIcon,
-  Button,
-  CurrencyIcon,
-} from "@ya.praktikum/react-developer-burger-ui-components";
-import ConstructorStyles from "./burger-constructor.module.css";
+ import { FoodPropTypes } from "../../utils/data";
+ import { arrayOf } from "prop-types";
+ import Modal from "../modal/modal";
+ import OrderDetails from "../order-details/order-details";
+ import {
+   ConstructorElement,
+   DragIcon,
+   Button,
+   CurrencyIcon,
+ } from "@ya.praktikum/react-developer-burger-ui-components";
+ import ConstructorStyles from "./burger-constructor.module.css";
 
-BurgerElement.propTypes = {
-  item: PropTypes.shape(FoodPropTypes),
-}
+ BurgerElement.propTypes = {
+   item: PropTypes.shape(FoodPropTypes),
+ };
 
-const BurgerConstructor = (props) => {
-  const [isOpen, setModal] = React.useState(false)
-  const close = ()=> {
-    setModal(false);
-  }
-  const openModal = () => {
-    setModal(true);
-  }
-  const [state] = useState({
-    items: props.data.map((item) =>
-      item.type !== "bun" ? <BurgerElement key={item._id} item={item} /> : ""
-    ),
-    bun: props.data.filter((item) => item.type === "bun"),
-  });
+ const BurgerConstructor = (props) => {
+   const [state, setState] = useState({
+     items: props.data.map((item) =>
+       item.type !== "bun" ? <BurgerElement key={item._id} item={item} /> : ""
+     ),
+     bun: props.data.filter((item) => item.type === "bun"),
+     modalOpen: false,
+   });
 
-  return (
-    <div className={ConstructorStyles.orderContainer}>
-      {state.bun[0] ? <BurgerElement item={state.bun[0]} type="top" /> : ""}
-      <ul className={ConstructorStyles.orderContent}>{state.items}</ul>
-      {state.bun[0] ? <BurgerElement item={state.bun[0]} type="bottom" /> : ""}
-      <div className={ConstructorStyles.priceContainer}>
-        <p className={ConstructorStyles.price}>
-          <span className="text text_type_digits-medium">610</span>
-          <CurrencyIcon type="primary" />
-        </p>
-        <Button type="primary" size="medium" onClick={openModal}>
-          Оформить заказ
-        </Button>
-      </div>
-      {isOpen && (
-      <Modal close={close}>
-        <OrderDetails />
-        </Modal>
-      )}
-    </div>
-  );
-};
+   const modalSwitch = () => {
+     setState({
+       ...state,
+       modalOpen: !state.modalOpen,
+       pickedItem: props,
+     });
+   };
 
-export default BurgerConstructor;
+   return (
+     <div className={ConstructorStyles.orderContainer}>
+       {state.bun[0] ? <BurgerElement item={state.bun[0]} type="top" /> : ""}
+       <ul className={ConstructorStyles.orderContent}>{state.items}</ul>
+       {state.bun[0] ? <BurgerElement item={state.bun[0]} type="bottom" /> : ""}
+       <div className={ConstructorStyles.priceContainer}>
+         <p className={ConstructorStyles.price}>
+           <span className="text text_type_digits-medium">610</span>
+           <CurrencyIcon type="primary" />
+         </p>
+         <Button type="primary" size="medium" onClick={modalSwitch}>
+           Оформить заказ
+         </Button>
+       </div>
+       {state.pickedItem && (
+       <Modal isOpen={state.modalOpen} close={modalSwitch}>
+         <OrderDetails />
+       </Modal>
+       )}
+     </div>
+   );
+ };
 
-function BurgerElement(ingredients) {
-  return (
-    <li className={ConstructorStyles.item}>
-      {ingredients.item.type !== "bun" ? (
-        <DragIcon type="primary"></DragIcon>
-      ) : (
-        <span></span>
-      )}
-      <ConstructorElement
-        type={ingredients.item.type === "bun" ? ingredients.type : ""}
-        isLocked={ingredients.item.type === "bun"}
-        text={ingredients.item.name}
-        price={ingredients.item.price}
-        thumbnail={ingredients.item.image}
-      ></ConstructorElement>
-    </li>
-  );
-}
+ export default BurgerConstructor;
 
-BurgerConstructor.propTypes = {
-  data: arrayOf(FoodPropTypes).isRequired,
-};
+ function BurgerElement(ingredients) {
+   return (
+     <li className={ConstructorStyles.item}>
+       {ingredients.item.type !== "bun" ? (
+         <DragIcon type="primary"></DragIcon>
+       ) : (
+         <span></span>
+       )}
+       <ConstructorElement
+         type={ingredients.item.type === "bun" ? ingredients.type : ""}
+         isLocked={ingredients.item.type === "bun"}
+         text={ingredients.item.name}
+         price={ingredients.item.price}
+         thumbnail={ingredients.item.image}
+       ></ConstructorElement>
+     </li>
+   );
+ }
+
+ BurgerConstructor.propTypes = {
+   data: arrayOf(FoodPropTypes).isRequired,
+ };
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,27 +1,29 @@
 import { useEffect } from "react";
-import { createPortal } from "react-dom";
-import ModalOverlay from "../modal-overlay/modal-overlay";
-import PropTypes from "prop-types";
-import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import ModalStyles from "./modal.module.css";
-const modals = document.querySelector("#modals");
+import { ReactDOM } from "react";
+ import { createPortal } from "react-dom";
+ import ModalOverlay from "../modal-overlay/modal-overlay";
+ import PropTypes from "prop-types";
+ import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+ import ModalStyles from "./modal.module.css";
+ const modals = document.querySelector("#modals");
 
-export default function Modal({children, close, isOpen}) {
-    useEffect(() => {
-      const handleEsc = (evt) => {
-        if (evt.key === "Escape") {
-          close();
-        }
-      };
-      if (isOpen) {
-        document.addEventListener("keydown", handleEsc);
-        return () => {
-          document.removeEventListener("keydown", handleEsc);
-        };
-      }
-    }, [isOpen, close]);
-  
-    return createPortal(
+ export default function Modal({children, close, isOpen}) {
+     useEffect(() => {
+       const handleEsc = (evt) => {
+         if (evt.key === "Escape") {
+           close();
+         }
+       };
+       if (isOpen) {
+         document.addEventListener("keydown", handleEsc);
+         return () => {
+           document.removeEventListener("keydown", handleEsc);
+         };
+       }
+     }, [isOpen, close]);
+
+     return (
+       isOpen && createPortal(
         <div className={ModalStyles.modal}>
           <div className={ModalStyles.popup}>
             <div className={ModalStyles.close} onClick={close}>
@@ -33,8 +35,9 @@ export default function Modal({children, close, isOpen}) {
         </div>,
         modals
       )
+    );
   };
-  
+
   Modal.propTypes = {
     children: PropTypes.node.isRequired,
     isOpen: PropTypes.bool.isRequired,
